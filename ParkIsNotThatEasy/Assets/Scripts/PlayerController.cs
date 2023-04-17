7853c926de5c5221d1a0c573a88ec72aaf81b4d2
ErikDRen <72856412+ErikDRen@users.noreply.github.com>
2023-04-17 17:42:44 +0200

    Player Controller
    cuve foward

---
+++ ParkIsNotThatEasy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float speed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
