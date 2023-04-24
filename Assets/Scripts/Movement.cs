66c65a55e04bd3fe711072f9134f30ce0f46edf3
Maxime ROADLEY-BATTIN <mroadleybattin02@gmail.com>
2023-04-24 17:18:52 +0200

    rotate avec touch

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,35 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _voiture;
-    
-    void Update()
+    public bool isTurningRight = false;
+    public bool isTurningLeft = false;
+
+    private void Update()
     {
-        if (Input.touchCount > 0) 
+        if (isTurningLeft == true)
         {
-            Touch touch = Input.GetTouch(0);
-            
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x,touch.position.y,10f));
-            Debug.Log(touchPosition);
-            Debug.DrawLine(Camera.main.transform.position, touchPosition, Color.red);
-
-            if (touchPosition.x > 2)
-            {
-                _voiture.transform.Rotate(0f, 1f, 0f);
-            }
-            //RaycastHit hit;
-            // if (Physics.Raycast(touchPosition, Camera.main.transform.forward, out hit, 100))
-            // {
-            //     Debug.Log(hit.point);
-            //     Debug.DrawLine(hit.point, Camera.main.transform.position, Color.red);
-            // }
-
-
-            //transform.position = touchPosition;
+            transform.Rotate(0f, -1f, 0f);
         }
+
+        if (isTurningRight == true)
+        {
+            transform.Rotate(0f, 1f, 0f);
+        }
+    }
+
+    public void UpdateTurnRightBool()
+    {
+        isTurningRight = !isTurningRight;
+    }
+    
+    public void UpdateTurnLeftBool()
+    {
+        isTurningLeft = !isTurningLeft;
     }
 }
