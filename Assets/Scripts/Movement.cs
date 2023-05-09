fff1e0cb133955dbe8e2a2e73211e581a5cd1be3
The Somwon <mroadleybattin02@gmail.com>
2023-05-09 11:49:15 +0200

    movement with deltatime

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,16 +8,17 @@ public class Movement : MonoBehaviour
     public bool isTurningRight = false;
     public bool isTurningLeft = false;
 
+    [SerializeField] private float _turningSpeed = 100f;
     private void Update()
     {
         if (isTurningLeft == true)
         {
-            transform.Rotate(0f, -1f, 0f);
+            transform.Rotate(0f, -(_turningSpeed * Time.deltaTime), 0f);
         }
 
         if (isTurningRight == true)
         {
-            transform.Rotate(0f, 1f, 0f);
+            transform.Rotate(0f, _turningSpeed * Time.deltaTime, 0f);
         }
     }
 
