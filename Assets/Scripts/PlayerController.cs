ba02daec0f152d4a1459d1fd26f8dc68cce2a79a
The Somwon <mroadleybattin02@gmail.com>
2023-05-16 09:53:11 +0200

    prout en vrai ce push change rien mais quand meme

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,15 +9,17 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     [SerializeField] Rigidbody _rb;
 
+    public int _vehiculeType; //1=tuture    2=navion    3=camtar
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
-            Vector3 speedY = transform.forward * speed;
-            speedY.y = _rb.velocity.y;
-            _rb.velocity = speedY;
-            //transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
+        Vector3 speedY = transform.forward * speed;
+        speedY.y = _rb.velocity.y;
+        _rb.velocity = speedY; 
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
     }
 }
